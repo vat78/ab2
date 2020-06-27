6ff5e112b3e02875a361cb3ebc2c5b61449b445e
Viacheslav_Tikhonov <Viacheslav_Tikhonov@epam.com>
2020-06-27 12:23:56 +0300

    Initial commit

---
+++ src/main/java/ru/vat78/alfa/ab2/Ab2Application.java
@@ -0,0 +1,13 @@
+package ru.vat78.alfa.ab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ab2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ab2Application.class, args);
+	}
+
+}
